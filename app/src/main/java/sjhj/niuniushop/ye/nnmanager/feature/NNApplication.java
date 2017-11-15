0db268bf30febae684eb094ae4cfa8fdd2352f7d
ye0yeg <ye0yeg@gmail.com>
2017-11-15 18:28:59 +0800

    物流查询

--- app/src/main/java/sjhj/niuniushop/ye/nnmanager/feature/NNApplication.java
+++ app/src/main/java/sjhj/niuniushop/ye/nnmanager/feature/NNApplication.java
@@ -13,10 +13,11 @@ public class NNApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //You Should Fear Me .!
         //82e2ea8e47d85e35fad6af646def27f9 是实际使用
         //337b791be8fac8168dede0d44fbd9003 是测试
 //        Bmob.initialize(this, "337b791be8fac8168dede0d44fbd9003"); //测试
-
         Bmob.initialize(this, "82e2ea8e47d85e35fad6af646def27f9"); //使用
     }
 }
